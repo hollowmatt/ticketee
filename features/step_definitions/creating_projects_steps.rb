efdef4b91446497f49401fd7362ccff4b2185a4b
Matt Holloway <matthew.holloway@logicwerx.com>
2011-11-09 12:48:26 -0500

    Creation of project feature complete... time to refactor

--- features/step_definitions/creating_projects_steps.rb
+++ features/step_definitions/creating_projects_steps.rb
@@ -21,3 +21,21 @@ end
 When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
   fill_in(field, :with => value)
 end
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => regexp)
+  else
+    assert page.has_xpath?('//*', :text => regexp)
+  end
+end
