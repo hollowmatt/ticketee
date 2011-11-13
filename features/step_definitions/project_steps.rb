7498ade166a325d331b16bf2d30b7f244e7d38ac
Matt Holloway <matthew.holloway@logicwerx.com>
2011-11-13 16:01:33 -0500

    Put redirect to home for users if a non-existing project is on url

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,3 +1,11 @@
 Given /^there is a project called "([^"]*)"$/ do |name|
   Factory(:project, :name => name)
 end
+
+Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should_not have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
